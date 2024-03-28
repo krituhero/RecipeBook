9bc656da8487df8e3a15919873bafb827ab1043f
Manish Dahal <dahalmanish2001@gmail.com>
2024-03-28 16:27:24 +0545

    Cass

--- RecipeBook/src/header.jsx
+++ RecipeBook/src/header.jsx
@@ -1,32 +1,25 @@
 import React from "react";
-import './App.css'
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
 
-
-
-export default function Header(){
-  return(
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/img/logo.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Link to='/about' className="about">About Us</Link>
-            <Link to='/cuisines' className="cuisine">Cuisine</Link>
-            <Link to='/popular' className="popular">Popular</Link>            
-          </Nav>
-        </Container>
-      </Navbar>
-  )
+export default function Header() {
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/img/logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          React Bootstrap
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
